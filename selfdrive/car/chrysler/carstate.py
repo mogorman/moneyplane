c93490a8ad6d20c7d82d1fe11d514593d584a0d9
Jonathan <jraycec@gmail.com>
2022-06-05 19:04:27 -0500

    ChryslerUpdate

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -26,12 +26,14 @@ class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]["pt"])
-    self.shifter_values = can_define.dv["GEAR"]["PRNDL"]
+
     self.cachedParams = CachedParams()
     self.opParams = opParams()
     self.lkasHeartbit = None
     self.dashboard = None
     self.speedRequested = 0
+
+    self.shifter_values = can_define.dv["TRANSMISSION_STATUS"]["GEAR_STATE"]
 
   def update(self, cp, cp_cam):
     min_steer_check = self.opParams.get('steer.checkMinimum')
@@ -39,20 +41,23 @@ class CarState(CarStateBase):
     ret = car.CarState.new_message()
 
     self.frame = int(cp.vl["EPS_STATUS"]["COUNTER"])
-
+    
+    # lock info 
     ret.doorOpen = any([cp.vl["DOORS"]["DOOR_OPEN_FL"],
                         cp.vl["DOORS"]["DOOR_OPEN_FR"],
                         cp.vl["DOORS"]["DOOR_OPEN_RL"],
                         cp.vl["DOORS"]["DOOR_OPEN_RR"]])
     ret.seatbeltUnlatched = cp.vl["SEATBELT_STATUS"]["SEATBELT_DRIVER_UNLATCHED"] == 1
 
-    ret.brakePressed = cp.vl["BRAKE_2"]["BRAKE_PRESSED_2"] == 5  # human-only
+    # brake pedal
+    ret.brakePressed = cp.vl["BRAKE_2"]["BRAKE_PRESSED_2"] ==1  # Physical brake pedal switch
     ret.brake = 0
-    ret.gas = cp.vl["ACCEL_GAS_134"]["ACCEL_134"]
+
+    # gas pedal
+    ret.gas = cp.vl["ACCEL_GAS_22F"]["ACCELERATOR_POSITION"]
     ret.gasPressed = ret.gas > 1e-5
 
-    ret.espDisabled = (cp.vl["TRACTION_BUTTON"]["TRACTION_OFF"] == 1)
-
+    # Speeds
     ret.wheelSpeeds = self.get_wheel_speeds(
       cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"],
       cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FR"],
@@ -60,37 +65,44 @@ class CarState(CarStateBase):
       cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_RR"],
       unit=1,
     )
-    ret.vEgoRaw = cp.vl["BRAKE_1"]["VEHICLE_SPEED_KPH"] * CV.KPH_TO_MS
+
+    ret.vEgoRaw = cp.vl["BRAKE_1"]["VEHICLE_SPEED"] * CV.KPH_TO_MS
+
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     ret.standstill = ret.vEgoRaw <= 0.1
 
+    # Button Presses
+    ret.espDisabled = (cp.vl["TRACTION_BUTTON"]["TRACTION_OFF"] == 1) # TODO: button is pressed. This doesn't mean ESP is diabled.
     ret.leftBlinker = cp.vl["STEERING_LEVERS"]["TURN_SIGNALS"] == 1
     ret.rightBlinker = cp.vl["STEERING_LEVERS"]["TURN_SIGNALS"] == 2
+    ret.genericToggle = bool(cp.vl["STEERING_LEVERS"]["HIGH_BEAM_FLASH"])
+
+    #Gear
+    ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl["TRANSMISSION_STATUS"]["GEAR_STATE"], None))
+
+    #Steering Info
     ret.steeringAngleDeg = cp.vl["STEERING"]["STEER_ANGLE"]
     ret.steeringRateDeg = cp.vl["STEERING"]["STEERING_RATE"]
-    ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl["GEAR"]["PRNDL"], None))
-
-    ret.cruiseState.enabled = cp.vl["ACC_2"]["ACC_ENABLED"] == 1  # ACC is green.
-    ret.cruiseState.available = cp.vl["DASHBOARD"]['CRUISE_STATE'] in [3, 4]  # the comment below says 3 and 4 are ACC mode
-    ret.cruiseState.speed = cp.vl["DASHBOARD"]["ACC_SPEED_CONFIG_KPH"] * CV.KPH_TO_MS
-    # CRUISE_STATE is a three bit msg, 0 is off, 1 and 2 are Non-ACC mode, 3 and 4 are ACC mode, find if there are other states too
-    ret.cruiseState.nonAdaptive = cp.vl["DASHBOARD"]["CRUISE_STATE"] in (1, 2)
-    self.dashboard = cp.vl["DASHBOARD"]
-
     ret.steeringTorque = cp.vl["EPS_STATUS"]["TORQUE_DRIVER"]
     ret.steeringTorqueEps = cp.vl["EPS_STATUS"]["TORQUE_MOTOR"]
     ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD
-    self.lkas_active = cp.vl["EPS_STATUS"]["LKAS_ACTIVE"] == 1
-    ret.steerError = cp.vl["EPS_STATUS"]["LKAS_STEER_FAULT"] == 1 or (min_steer_check and not self.lkas_active and ret.vEgo > self.CP.minSteerSpeed)
 
-    ret.genericToggle = bool(cp.vl["STEERING_LEVERS"]["HIGH_BEAM_FLASH"])
+    steer_state = cp.vl["EPS_STATUS"]["LKAS_STATE"]
+    self.button_counter = cp.vl["WHEEL_BUTTONS"]["COUNTER"]
 
     if self.CP.enableBsm:
       ret.leftBlindspot = cp.vl["BLIND_SPOT_WARNINGS"]["BLIND_SPOT_LEFT"] == 1
       ret.rightBlindspot = cp.vl["BLIND_SPOT_WARNINGS"]["BLIND_SPOT_RIGHT"] == 1
 
+    ret.cruiseState.enabled = cp.vl["ACC_2"]["ACC_ENGAGED"] == 1  # ACC is green.
+    ret.cruiseState.available = cp.vl["ACC_2"]["ACC_ENABLED"] == 1
+    ret.cruiseState.speed = cp.vl["DASHBOARD"]["ACC_SPEED_CONFIG_KPH"] * CV.KPH_TO_MS
+    # CRUISE_STATE is a three bit msg, 0 is off, 1 and 2 are Non-ACC mode, 3 and 4 are ACC mode, find if there are other states too
+    ret.cruiseState.nonAdaptive = cp.vl["DASHBOARD"]["CRUISE_STATE"] in (1, 2)
+    ret.accFaulted = cp.vl["ACC_2"]["ACC_FAULTED"] != 0
     self.lkas_counter = cp_cam.vl["LKAS_COMMAND"]["COUNTER"]
     self.lkas_car_model = cp_cam.vl["LKAS_HUD"]["CAR_MODEL"]
+
     self.torq_status = cp.vl["EPS_STATUS"]["TORQ_STATUS"]
 
     brake = cp.vl["BRAKE_1"]["BRAKE_VAL_TOTAL"]
@@ -110,6 +122,9 @@ class CarState(CarStateBase):
     for buttonType in CHECK_BUTTONS:
       self.check_button(button_events, buttonType, bool(cp.vl[CHECK_BUTTONS[buttonType][0]][CHECK_BUTTONS[buttonType][1]]))
     ret.buttonEvents = button_events
+
+    self.lkas_status_ok = cp_cam.vl["LKAS_HEARTBIT"]["LKAS_STATUS_OK"]
+    ret.steerFaultPermanent =  cp.vl["EPS_STATUS"]["LKAS_STEER_FAULT"]  == 1 or (min_steer_check and not cp.vl["EPS_STATUS"]["LKAS_ACTIVE"] and ret.vEgo > self.CP.minSteerSpeed)
 
     return ret
 
@@ -143,14 +158,15 @@ class CarState(CarStateBase):
   @staticmethod
   def get_can_parser(CP):
     signals = [
-      # sig_name, sig_address, default
-      ("PRNDL", "GEAR"),
+      # sig_name, sig_address
+      ("GEAR_STATE", "TRANSMISSION_STATUS"),
       ("DOOR_OPEN_FL", "DOORS"),
       ("DOOR_OPEN_FR", "DOORS"),
       ("DOOR_OPEN_RL", "DOORS"),
       ("DOOR_OPEN_RR", "DOORS"),
       ("BRAKE_PRESSED_2", "BRAKE_2"),
-      ("ACCEL_134", "ACCEL_GAS_134"),
+      ("ACCELERATOR_POSITION", "ACCEL_GAS_22F"),
+      ("VEHICLE_SPEED", "BRAKE_1"),
       ("WHEEL_SPEED_FL", "WHEEL_SPEEDS"),
       ("WHEEL_SPEED_RR", "WHEEL_SPEEDS"),
       ("WHEEL_SPEED_RL", "WHEEL_SPEEDS"),
@@ -158,7 +174,9 @@ class CarState(CarStateBase):
       ("STEER_ANGLE", "STEERING"),
       ("STEERING_RATE", "STEERING"),
       ("TURN_SIGNALS", "STEERING_LEVERS"),
+      ("ACC_ENGAGED", "ACC_2"),
       ("ACC_ENABLED", "ACC_2"),
+      ("ACC_FAULTED", "ACC_2"),
       ("HIGH_BEAM_FLASH", "STEERING_LEVERS"),
       ("ACC_SPEED_CONFIG_KPH", "DASHBOARD"),
       ("CRUISE_STATE", "DASHBOARD"),
@@ -194,17 +212,15 @@ class CarState(CarStateBase):
       ("WHEEL_SPEEDS", 50),
       ("STEERING", 100),
       ("ACC_2", 50),
-      ("GEAR", 50),
-      ("ACCEL_GAS_134", 50),
+      ("TRANSMISSION_STATUS", 50),
+      ("ACCEL_GAS_22F", 50),
+      ("WHEEL_BUTTONS", 50),
       ("DASHBOARD", 15),
       ("STEERING_LEVERS", 10),
       ("SEATBELT_STATUS", 2),
       ("DOORS", 1),
       ("TRACTION_BUTTON", 1),
-      ("WHEEL_BUTTONS", 50),
-      ("BLIND_SPOT_WARNINGS", 2),
-      ("BRAKE_1", 100),
-      ("ACCEL_RELATED_120", 50)
+      ("BRAKE_1", 50),
     ]
 
     if CP.enableBsm:
